f96a3c4a56e07f305e8b58c99685713da06db56d
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-22 22:15:36 +0530

    SCANNEROVER

--- First.java
+++ First.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class First {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         System.out.println("hello world");
 
-    }
+        int age ;
+        System.out.println("enter age");
+        Scanner x;
+        x= new Scanner(System.in);
+        age =x.nextInt();
+        if(age>20){
+            System.out.println("you are older than 20");
+        }
+        else {
+            System.out.println("you are younger  than 20");
+        }
+
+        }
+
+
 }
