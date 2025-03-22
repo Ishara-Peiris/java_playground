5b9cb43185c53fc0afb3793e9d0c1447dc7f9348
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-22 22:32:02 +0530

    SWITCH_OVER

--- First.java
+++ First.java
@@ -4,19 +4,44 @@ public class First {
     public static void main(String[]args) {
         System.out.println("hello world");
 
-        int age ;
+        int age;
         System.out.println("enter age");
         Scanner x;
-        x= new Scanner(System.in);
-        age =x.nextInt();
-        if(age>20){
+        x = new Scanner(System.in);
+        age = x.nextInt();
+        if (age > 20) {
             System.out.println("you are older than 20");
-        }
-        else {
+        } else {
             System.out.println("you are younger  than 20");
         }
+        int dayNumber;
+        System.out.println("enter day number");
+        Scanner y;
+        y = new Scanner(System.in);
+        dayNumber = y.nextInt();
+        switch (dayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+                case 4:System.out.println("thursday");
+                break;
+            case 5:System.out.println("friday");
+                break;
+                case 6:System.out.println("saturday");
+                break;
+
+                case 7:System.out.println("sunday");
+            default:
+                System.out.println("invalid day number");
 
         }
+    }
 
 
-}
+    }
